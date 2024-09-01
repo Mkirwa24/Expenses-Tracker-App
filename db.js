cbf3b0c06ce0f35d735e9df29395d38eccb53f5c
Mathias Kirwa <mkirwa24@gmail.com>
2024-09-01 17:34:09 +0300

    Expense tracker App

--- db.js
+++ db.js
@@ -3,19 +3,16 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createConnection({
+// Create a connection pool
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    
+    database: process.env.DB_NAME, // Make sure to add this if you have a database name
+    waitForConnections: true,      // Wait for a connection to become available
+    connectionLimit: 10,           // Number of connections to create in the pool
+    queueLimit: 0                  // Limit on the number of connection requests
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the database:', db.threadId);
-});
-
-module.exports = db;
+// Export the pool for use in other parts of the application
+module.exports = pool.promise();  // Use promise-based API for async/await
