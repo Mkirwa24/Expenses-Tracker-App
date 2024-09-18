462f0e509e121942d1a9be01863a188c9eb74e62
Mathias Kirwa <mkirwa24@gmail.com>
2024-09-18 12:28:15 +0300

    Expenses Tracker App

--- server.js
+++ server.js
@@ -45,8 +45,10 @@ app.get('/', (req, res) => {
          id INT AUTO_INCREMENT PRIMARY KEY,
          email VARCHAR(100) NOT NULL UNIQUE,
          username VARCHAR(255) NOT NULL UNIQUE,
-         password VARCHAR(255) NOT NULL
-     )
+         password VARCHAR(255) NOT NULL,
+         resetToken VARCHAR(255),       
+         resetTokenExpiry BIGINT        
+         )
  `;
  db.query(createUsersTable, (err) => {
      if (err) {
