a2bdc89a1ca4a81a0f3282009b79976d34548b14
Mathias Kirwa <mkirwa24@gmail.com>
2024-09-20 19:30:23 +0300

    Expense Tracker Application

--- public/js/forgot-password.js
+++ public/js/forgot-password.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
         const email = document.getElementById('email').value;
+        const securityAnswer = document.getElementById('securityAnswer').value;
 
         try {
             const response = await fetch('https://expenses-tracking-application1.onrender.com/forgot-password', {
@@ -11,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ email })
+                body: JSON.stringify({ email, securityAnswer})
             });
 
             const data = await response.json();
@@ -22,6 +23,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Redirect to the reset password page with token in the query string
                 const resetPasswordUrl = `/reset-password?token=${data.token}`;
                 window.location.href = resetPasswordUrl;
+
+                } else if (response.status === 404) {
+                // User not found
+                alert(data.message || 'No user found with this email address.');
+            } else if (response.status === 401) {
+                // Incorrect security answer
+                alert(data.message || 'Incorrect security answer. Please try again.');
+
+            } else if (response.status === 429) {
+                // Handle too many reset attempts
+                alert(data.message || 'Too many reset attempts. Please try again later.');
+                } else if (response.status === 500) {
+                // Internal server error
+                alert(data.message || 'A server error occurred. Please try again later.');
             } else {
                 // Display error message
                 alert(data.message || 'Password reset failed.');
